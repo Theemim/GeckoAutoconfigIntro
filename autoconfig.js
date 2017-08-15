fbeae731337ac58cf6e9d243510710309e55ab9e
Theemim <Theemim@users.noreply.github.com>
2017-08-15 00:38:24 -0400

    Update autoconfig.js

--- autoconfig.js
+++ autoconfig.js
@@ -1,4 +1,4 @@
-// First line is ignore.  Leave a single line comment here.
+// First line is ignored.  Leave a single line comment here.
 
 pref("general.config.obscure_value", 0);
 pref("general.config.filename", "testcfg.js");
