2bf53704703694d08721974384fedbd4d55d6f11
Theemim <Theemim@users.noreply.github.com>
2017-08-16 18:25:16 -0400

    Add files via upload

--- testcfg.js
+++ testcfg.js
@@ -11,10 +11,11 @@ try {
     lockPref("__testcfgMilestone", str);
   }
   setMilestone("1");
+
   if(typeof(Services) === "undefined") {
     Components.utils.import("resource://gre/modules/Services.jsm");
   }
-  function generateError() {
+  function createErrorCondition() {
     // Throw of new Error
     throw(new Error("This is a throw of a new Error"));
 
@@ -68,52 +69,120 @@ try {
     });
     return(str + "\n");
   }
-  function confirmEx(title, msg, button0Title, button1Title, button2Title) {
-    // button1 should be the safe cancel/donothing option
+  function buttonPrompt(title, msg, button0Title, button1Title, button2Title) {
+    // Due to bug 345067, this will always return 1 if the user closes the window
+    // using the close button in the titlebar.  Behavior of this function: button1
+    // is non-optional, it will always be the default, if it is clicked or a close
+    // happens then the return code will be 1.  A return code of 1 should be a safe
+    // "do nothing" choice.  Other buttons are optional.  If only two buttons are
+    // needed use button0 and button1.  Button positions may be platform dependent.
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XPCOM/Reference/Interface/nsIPromptService#confirmEx()
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=345067
+    if(!button1Title) {
+      throw(new Error("button1 is not optional"));
+    }
+    if(!button0Title && button2Title) {
+      throw(new Error("For two button prompts use button0 and button1"));
+    }
+    var buttonFlags;
     var promptSvc = Services.prompt;
-    var buttonFlags = (promptSvc.BUTTON_POS_1 * promptSvc.BUTTON_TITLE_IS_STRING) +
-                       promptSvc.BUTTON_POS_1_DEFAULT;
-    if(button0Title != null) {
-      buttonFlags += (promptSvc.BUTTON_POS_0 * promptSvc.BUTTON_TITLE_IS_STRING);
+    if(!button0Title && !button2Title) {
+       // Adjustment due to button1 handling
+      button0Title = button1Title;
+      button1Title = null;
+      buttonFlags = (promptSvc.BUTTON_POS_0 * promptSvc.BUTTON_TITLE_IS_STRING) +
+                     promptSvc.BUTTON_POS_0_DEFAULT;
     }
-    if(button2Title !== null) {
-      buttonFlags += (promptSvc.BUTTON_POS_2 * promptSvc.BUTTON_TITLE_IS_STRING);
+    else {
+      buttonFlags = (promptSvc.BUTTON_POS_1 * promptSvc.BUTTON_TITLE_IS_STRING) +
+                     promptSvc.BUTTON_POS_1_DEFAULT;
+      if(button0Title) {
+        buttonFlags += (promptSvc.BUTTON_POS_0 * promptSvc.BUTTON_TITLE_IS_STRING);
+      }
+      if(button2Title) {
+        buttonFlags += (promptSvc.BUTTON_POS_2 * promptSvc.BUTTON_TITLE_IS_STRING);
+      }
     }
-    return(promptSvc.confirmEx(null, title, msg, buttonFlags, button0Title, button1Title, button2Title, null, {}));
+    var result = promptSvc.confirmEx(null, title, msg, buttonFlags, button0Title, button1Title, button2Title, null, {});
+    if((!button1Title && !button2Title) && (result === 0)) {
+       // Adjustment due to button1 handling
+       result = 1;
+    }
+    return(result);
+  }
+  function getDialogMsg(leader, trailer) {
+    if(leader) {
+      leader += "\n\n";
+    }
+    return(leader + getTestPrefsStr() + trailer + "\n\n");
   }
   setMilestone("2");
-  var greeting = "Hello " + (getenv("USERNAME") || getenv("USER") || "Human") + "!\n\n";
-  var msg = greeting + getTestPrefsStr();
-  var buttonPressed = confirmEx(cfgFile, msg, "Test error handling", "Next", "Set testcfg prefs");
+
+  var leader = "Hello " + (getenv("USERNAME") || getenv("USER") || "Human") + "!";
+  var msg = getDialogMsg(leader, "Would you like to create an error condition?");
+  var buttonPressed = buttonPrompt(cfgFile, msg, "Create error", "Skip", null);
   if(buttonPressed === 0) {
-    generateError();
-    msg = "You shouldn't see this";
+    createErrorCondition();
+    leader = "An error condition was created, so you should not see this";
   }
-  else if(buttonPressed === 1) {
-    msg = "What would you like to do now?";
-  }
-  else if(buttonPressed === 2) {
-    pref("__testcfg-setwith-pref", "FOUND");
-    defaultPref("__testcfg-setwith-defaultPref", "FOUND");
-    lockPref("__testcfg-setwith-lockPref", "FOUND");
-    msg = "The testcfg prefs were set.\n\n" + getTestPrefsStr();
-  }
+  else leader = "";
   setMilestone("3");
-  buttonPressed = confirmEx(cfgFile, msg, "Clear all test prefs", "Next", null);
-  msg = "";
+
+  msg = getDialogMsg(leader, "Would you like to set testcfg prefs?");
+  buttonPressed = buttonPrompt(cfgFile, msg, "Set testcfg prefs", "Skip", null);
+  if(buttonPressed === 0) {
+    pref("__testcfg-setwith-pref", "Set by testcfg.js");
+    defaultPref("__testcfg-setwith-defaultPref", "Set by testcfg.js");
+    lockPref("__testcfg-setwith-lockPref", "Set by testcfg.js");
+    leader = "The testcfg prefs were set.";
+  }
+  else leader = "";
+  setMilestone("3");
+
+  msg = getDialogMsg(leader, "Would you like to set userjs prefs?");
+  buttonPressed = buttonPrompt(cfgFile, msg, "Set userjs prefs", "Skip", null);
+  if(buttonPressed === 0) {
+    msg = getDialogMsg("", "Which function do you want to use to set userjs prefs?");
+    buttonPressed = buttonPrompt(cfgFile, msg, "pref()", "lockPref()", "defaultPref()");
+    var prefSetFunc;
+    var prefValue;
+    if(buttonPressed === 0) {
+      prefSetFunc = pref;
+      prefValue = "Set by testcfg.js with pref";
+    }
+    else if(buttonPressed === 1) {
+      prefSetFunc = lockPref;
+      prefValue = "Set by testcfg.js with lockPref";
+    }
+    else  {
+      prefSetFunc = defaultPref;
+      prefValue = "Set by testcfg.js with defaultPref";
+    }
+    prefSetFunc("__userjs-setwith-user_pref", prefValue);
+    prefSetFunc("__userjs-setwith-pref", prefValue);
+    prefSetFunc("__userjs-setwith-sticky_pref", prefValue);
+    leader = "The testcfg prefs were set.";
+  }
+  setMilestone("4");
+
+  msg = getDialogMsg(leader, "Would you like to clear all test prefs?");
+  buttonPressed = buttonPrompt(cfgFile, msg, "Clear all test prefs", "Skip", null);
   if(buttonPressed === 0) {
     testPrefs.forEach(function(prefName) {
       clearPref(prefName);
     });
-    msg = "All test prefs were cleared.\n\n";
+    leader = "All test prefs were cleared.";
   }
-  setMilestone("4");
-  msg += getTestPrefsStr();
-  Services.prompt.alert(null, cfgFile, msg);
+  else leader = "";
+  setMilestone("5");
+
+  msg = getDialogMsg(leader, "Nothing more to do.");
+  buttonPrompt(cfgFile, msg, null, "OK", null);
+
   var consoleSvc = Components.classes["@mozilla.org/consoleservice;1"]
                              .getService(Components.interfaces.nsIConsoleService);
   consoleSvc.logStringMessage(cfgFile + " was here");
-  setMilestone("5 (Done)");
+  setMilestone("6 (Done)");
 }
 catch(e) {
   displayError(cfgFile, "\n\n " + e.toString() + "\n Stack: " + e.stack +
--- user.js
+++ user.js
@@ -8,25 +8,25 @@ user_pref("__userjsMilestone", "1");
 
 // Setting an int pref to a string
 //user_pref("__userjs-error2", 1);
-//user_pref("__userjs-error2", "FOUND");
+//user_pref("__userjs-error2", "this-should-not-be-set");
 
 // Missing semicolon
-//user_pref("__userjs-error3", "FOUND")
+//user_pref("__userjs-error3", "this-should-not-be-set")
 
 // Missing closing quote
-//user_pref("__userjs-error4, "FOUND");
+//user_pref("__userjs-error4, "this-should-not-be-set");
 
 // Extra paren
-//user_pref("__userjs-error5", "FOUND"));
+//user_pref("__userjs-error5", "this-should-not-be-set"));
 
 // Unmatched closing brace
 // }
 
 user_pref("__userjsMilestone", "2");
 
-user_pref("__userjs-setwith-user_pref", "FOUND");
-pref("__userjs-setwith-pref", "FOUND");
-sticky_pref("__userjs-setwith-sticky_pref", "FOUND");
+user_pref("__userjs-setwith-user_pref", "Set by user.js");
+pref("__userjs-setwith-pref", "Set by user.js");
+sticky_pref("__userjs-setwith-sticky_pref", "Set by user.js");
 
 user_pref("__userjsMilestone", "3 (Done)");
 
