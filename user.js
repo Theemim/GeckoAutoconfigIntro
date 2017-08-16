cdb84c0987abde662b66009bcbe0a69fa5913eb2
Theemim <Theemim@users.noreply.github.com>
2017-08-15 22:08:29 -0400

    Add files via upload

--- user.js
+++ user.js
@@ -7,26 +7,26 @@ user_pref("__userjsMilestone", "1");
 //user_pref("__userjs-error1");
 
 // Setting an int pref to a string
-//lockPref("__userjs-error2", 1);
-//lockPref("__userjs-error2", "FOUND");
+//user_pref("__userjs-error2", 1);
+//user_pref("__userjs-error2", "FOUND");
 
 // Missing semicolon
-//lockPref("__userjs-error3", "FOUND")
+//user_pref("__userjs-error3", "FOUND")
 
 // Missing closing quote
-//lockPref("__userjs-error4, "FOUND");
+//user_pref("__userjs-error4, "FOUND");
 
 // Extra paren
-//lockPref("__userjs-error5", "FOUND"));
+//user_pref("__userjs-error5", "FOUND"));
 
 // Unmatched closing brace
 // }
 
 user_pref("__userjsMilestone", "2");
 
-pref("__userjs-setwith-user_pref", "FOUND");
+user_pref("__userjs-setwith-user_pref", "FOUND");
 pref("__userjs-setwith-pref", "FOUND");
-pref("__userjs-setwith-sticky_pref", "FOUND");
+sticky_pref("__userjs-setwith-sticky_pref", "FOUND");
 
 user_pref("__userjsMilestone", "3 (Done)");
 
